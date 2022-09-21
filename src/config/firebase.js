b61a9486425a81d8ccddc33f3bdd4af95ce51ba6
yeremiavictor <yeremiavictor@gmail.com>
2022-09-21 14:53:17 +0700

    create connection firestore & auth

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {getAuth} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYyyVYUHzT4dtro4hYCw9CCIdYWOwNtTQ",
+  authDomain: "project-auth-8da35.firebaseapp.com",
+  projectId: "project-auth-8da35",
+  storageBucket: "project-auth-8da35.appspot.com",
+  messagingSenderId: "1012470831613",
+  appId: "1:1012470831613:web:3905e9e554ba96114c6194",
+  measurementId: "G-GMD9LF3DX5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const dbd = getFirestore(app)
+
+export const auth = getAuth(app)
