0481c1e9af291e0756a73a2038649f5d40d01f60
yeremiavictor <yeremiavictor@gmail.com>
2022-09-22 11:18:42 +0700

    create footer design

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer(){
+    return(
+        <div className="content has-text-centered">
+            <p>Yeremia Victor © {new Date().getFullYear()}</p>
+        </div>
+    )
+}
