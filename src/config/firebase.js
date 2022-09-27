8349cd6493fc9af1f32c567ae59486a64ee91348
yeremiavictor <yeremiavictor@email.com>
2022-09-27 15:58:06 +0700

    repair misstype (db)

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -14,6 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const dbd = getFirestore(app)
+export const db = getFirestore(app)
 
 export const auth = getAuth(app)
